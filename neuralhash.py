642895240eda35de841c2b82025bb7e9a8dabed3
zm <zm10162021@126.com>
2023-06-21 09:25:15 +0800

    V2:     data shape：[400000,512]     model     : 4层全连接层     loss      : pow(clamp(self.margin - euclidean_distance, min))     optimizer : Adam     result    : v2_40_Adam.png     describe  : loss依旧动荡，在一个值附近跳跃，rate0.16，length 7Y

--- neuralhash.py
+++ neuralhash.py
@@ -18,18 +18,15 @@ class HashFunction(nn.Module):
         self.fc3 = nn.Linear(512, 256)
         self.bn3 = nn.BatchNorm1d(256)
         self.fc4 = nn.Linear(256, 128)
-        self.bn4 = nn.BatchNorm1d(128)
-        self.fc5 = nn.Linear(128, 64)
-        self.fc6 = nn.Linear(64, 128)
 
     def forward(self, x):
         x = torch.relu(self.bn1(self.fc1(x)))
         x = torch.relu(self.bn2(self.fc2(x)))
         x = torch.relu(self.bn3(self.fc3(x)))
-        x = torch.relu(self.bn4(self.fc4(x)))
-        x = torch.relu(self.fc5(x))
-        x = torch.sigmoid(self.fc6(x))
+        x = torch.sigmoid((self.fc4(x)))
+
         return x
+
 
 
 # Define the dataset
@@ -43,29 +40,21 @@ class HashDataset(Dataset):
     def __getitem__(self, idx):
         return self.data1[idx],self.data2[idx]
 
-def dis_criterion(output1,output2):
-    distance = torch.dist(output1, output2,p = 2)
-    loss = - torch.mean(distance)
-    return loss
-'''
-# Generate random data and labels
-train_num,test_num = 990000, 10000 
-changenum = 5
-data = [''.join(np.random.choice(['0','1'], 512)) for _ in range(train_num + test_num)]
+# Contrastive loss
+class ContrastiveLoss(nn.Module):
+    def __init__(self, margin):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
 
-similar_data = []
-#random choose a bit to change
-for d in data:
-    indices = random.sample(list(range(512)),changenum)
-    d_list = list(d)
-    for i in indices:
-        d_list[i] = '0' if d[i] == '1' else '1'
-    similar_data.append(''.join(d_list))
+    def forward(self, output1, output2):
+        euclidean_distance = torch.pairwise_distance(output1, output2)
+        #loss_contrastive = torch.mean(torch.relu(self.margin - euclidean_distance))
+        loss_contrastive = torch.mean(torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        #loss_contrastive = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) + label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        return loss_contrastive
 
-data = torch.tensor([list(map(int, d)) for d in data], dtype=torch.float32)
-similar_data = torch.tensor([list(map(int, d)) for d in similar_data], dtype=torch.float32)
-'''
-train_num, test_num = 1990000, 10000
+
+train_num, test_num = 390000, 10000
 data_path = 'data.npy'
 
 # load data
@@ -77,15 +66,19 @@ print(data.shape,similar_data.shape)
 
 # Create the dataset and dataloader
 train_dataset = HashDataset(data[:train_num],similar_data[:train_num])
-train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
 # Initialize the model, loss function, and optimizer
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 model = HashFunction().to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+#optimizer = optim.SGD(model.parameters(), lr=0.001)
+margin = 128
+criterion = ContrastiveLoss(margin)
+
 # Train the model
-num_epochs = 1
+num_epochs = 100
 test_data = data[train_num:].to(device)
 s_test_data = similar_data[train_num:].to(device)
 threshold = 10
@@ -93,15 +86,15 @@ loss_list = []
 dist_list = []
 rate = []
 for epoch in range(num_epochs):
-    #optimizer.zero_grad()
     for batch_idx, (p_data, s_data) in enumerate(train_dataloader):
         p_data = p_data.to(device)
         s_data = s_data.to(device)
         
-        optimizer.zero_grad()
+        
         outputs1 = model(p_data)
         outputs2 = model(s_data)
-        loss = dis_criterion(outputs1, outputs2)
+        loss = criterion(outputs1, outputs2)
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
     loss_list.append(loss.item())
@@ -119,9 +112,7 @@ for epoch in range(num_epochs):
             distances.append(distance.item())
             if distance > threshold:
                 qualified += 1
-            if distance > 128:
-                print(output1)
-                print(output2)
+
         avg_distance = np.mean(distances)
         dist_list.append(avg_distance)
         rate.append(qualified/test_num)
@@ -153,4 +144,4 @@ plt.grid(color='k', linestyle=':')
 plt.legend()
 
 plt.imshow
-plt.show()  
+plt.show() 
